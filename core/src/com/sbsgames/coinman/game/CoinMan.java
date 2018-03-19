09387886117192265cb6f7587c73db434806bbd8
Joe Ortega <joeortega79@gmail.com>
2018-03-20 08:01:02 +1030

    Added sprites to the project.  Player appears and jumps.  Coins and bombs spawn and scroll across the screen.

--- core/src/com/sbsgames/coinman/game/CoinMan.java
+++ core/src/com/sbsgames/coinman/game/CoinMan.java
@@ -2,32 +2,118 @@ package com.sbsgames.coinman.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class CoinMan extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import java.util.ArrayList;
+import java.util.Random;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class CoinMan extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture background, coin, bomb;
+    Texture[] man;
+    int manState = 0, pause = 0;
+    float gravity = 0.2f, velocity = 0;
+    int manY = 0;
+
+    ArrayList<Integer> coinXs = new ArrayList();
+    ArrayList<Integer> coinYs = new ArrayList();
+    int coinCount = 0;
+
+    ArrayList<Integer> bombXs = new ArrayList();
+    ArrayList<Integer> bombYs = new ArrayList();
+    int bombCount = 0;
+
+    Random random;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        man = new Texture[4];
+        man[0] = new Texture("frame-1.png");
+        man[1] = new Texture("frame-2.png");
+        man[2] = new Texture("frame-3.png");
+        man[3] = new Texture("frame-4.png");
+
+        manY = Gdx.graphics.getHeight() / 2;
+
+        coin = new Texture("coin.png");
+        bomb = new Texture("bomb.png");
+
+        random = new Random();
+    }
+
+    @Override
+    // Draws back to front, the highest number is the top layer
+    // 1 - background | 2 - Coin | 3 - Bomb | 4 - Man
+    public void render() {
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if (coinCount < 100) {
+            coinCount++;
+        } else {
+            coinCount = 0;
+            makeCoin();
+        }
+
+        for (int i = 0; i < coinXs.size(); i++) {
+            batch.draw(coin, coinXs.get(i), coinYs.get(i));
+            coinXs.set(i, coinXs.get(i) - 4);
+        }
+
+        if (bombCount < 300) {
+            bombCount++;
+        } else {
+            bombCount = 0;
+            makeBomb();
+        }
+
+        for (int i = 0; i < bombXs.size(); i++) {
+            batch.draw(bomb, bombXs.get(i), bombYs.get(i));
+            bombXs.set(i, bombXs.get(i) - 4);
+        }
+
+        if (Gdx.input.justTouched()) {
+            velocity = -10;
+        }
+
+        if (pause < 8) {
+            pause++;
+        } else {
+            pause = 0;
+            if (manState < 3) {
+                manState++;
+            } else {
+                manState = 0;
+            }
+        }
+        velocity += gravity;
+        manY -= velocity;
+
+        if (manY <= 0) {
+            manY = 0;
+        }
+
+        batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[0].getWidth() / 2, manY);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    private void makeCoin() {
+        float height = random.nextFloat() * Gdx.graphics.getHeight();
+        coinYs.add((int) height);
+        coinXs.add(Gdx.graphics.getWidth());
+    }
+
+    private void makeBomb() {
+        float height = random.nextFloat() * Gdx.graphics.getHeight();
+        bombYs.add((int) height);
+        bombXs.add(Gdx.graphics.getWidth());
+    }
 }
